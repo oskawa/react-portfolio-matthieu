0c1cd2e06c62e01dcce592f9140278eaaec8502c
Maxime <maxime.eloir@agenceseize.fr>
2024-04-07 20:02:27 +0200

    Change URL to https baseUrl api

--- src/api/http.ts
+++ src/api/http.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const http = axios.create({
-  baseURL: 'http://portfolio-matthieu-back.maxime-eloir.fr/wp-json/matthieu-delomez/v1/',
+  baseURL: 'https://portfolio-matthieu-back.maxime-eloir.fr/wp-json/matthieu-delomez/v1/',
 })
 
 export default http
